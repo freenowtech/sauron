83c325cda8ada2812cb7dea602203fdc6a94b0e9
sergio.santiago <sergio.a.santiago@gmail.com>
2021-03-24 16:10:16 +0100

    Open sourcing sauron-core module

---
+++ sauron-core/src/main/java/com/freenow/sauron/plugins/SauronPlugin.java
@@ -0,0 +1,11 @@
+package com.freenow.sauron.plugins;
+
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+
+public class SauronPlugin extends Plugin
+{
+    public SauronPlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+}
