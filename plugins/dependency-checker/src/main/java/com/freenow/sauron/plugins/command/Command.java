e4066ff1a69c0e3533abeedb5aee7e2c91cfe2d1
Cristian Silva <cristian.silva@mytaxi.com>
2022-12-10 23:19:27 +0100

    dependency-checker - resolve the issue [Bump of packaging-21.3 to packaging-22.0 breaks cyclonedx-python](https://github.com/CycloneDX/cyclonedx-python/issues/449) on [packaging](https://github.com/pypa/packaging/issues/631) which prevents cyclonedx to generate bom.xml

--- plugins/dependency-checker/src/main/java/com/freenow/sauron/plugins/command/Command.java
+++ plugins/dependency-checker/src/main/java/com/freenow/sauron/plugins/command/Command.java
@@ -5,17 +5,22 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+@Slf4j
 @Builder
 public class Command
 {
 
     private final Map<String, String> environment;
     private final File outputFile;
-    private final String commandline;
+    private final List<String> commandline;
     private final Path repositoryPath;
     private final Integer commandTimeout;
 
@@ -23,18 +28,25 @@ public class Command
     public void run() throws IOException, InterruptedException, NonZeroExitCodeException
     {
         ProcessBuilder builder = new ProcessBuilder()
-            .command(commandline.split("\\s"))
+            .command(commandline.get(0), commandline.get(1))
             .directory(repositoryPath.toFile());
 
         if (environment != null && !environment.isEmpty())
         {
             builder.environment().putAll(environment);
         }
+        log.info(builder.command().toString());
         Process process = builder.start();
 
         if (!process.waitFor(commandTimeout, TimeUnit.MINUTES))
         {
-            throw new NonZeroExitCodeException(commandline, new String(process.getErrorStream().readAllBytes()));
+            throw new NonZeroExitCodeException(commandline.toString(), new String(process.getErrorStream().readAllBytes()));
+        }
+
+        String processLogs = new String(process.getErrorStream().readAllBytes());
+        if (isNotBlank(processLogs))
+        {
+            log.info(processLogs);
         }
 
         if (outputFile != null)
